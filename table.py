68b2bec9acd224ffe0d524a9ec0318ea695b3330
changwang29 <102817945+changwang29@users.noreply.github.com>
2022-10-30 23:47:27 -0400

    Add files via upload
    I'm having trouble uploading files from vs code. You need to put table.html in a folder named "templates" in order to work

---
+++ table.py
@@ -0,0 +1,27 @@
+# importing flask
+from flask import Flask, render_template
+
+# importing pandas module
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+# reading the data in the csv file
+# df = pd.read_csv('output_data.csv')
+# df.to_csv('output_data.csv', index=None)
+
+
+# route to html page - "table"
+@app.route('/')
+@app.route('/table')
+def table():
+	
+	# converting csv to html
+	data = pd.read_csv('output_data.csv')
+	return render_template('table.html', tables=[data.to_html()], titles=[''])
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
